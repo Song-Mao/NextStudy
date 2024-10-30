6475128eaa8182feb7e74aa5182e3af51589c968
18565646014 <468256399@qq.com>
2024-10-30 22:20:29 +0800

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,17 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  console.log('middleware');
   const path = request.nextUrl.pathname;
   const token = request.cookies.get('token')?.value;
   const isAuthenticated = !!token;
 
-  // 路由守卫
   if (path === '/' && !isAuthenticated) {
     return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if(path === '/login' && isAuthenticated){
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
